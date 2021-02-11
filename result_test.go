77a13a391c0472786afc0bfed69ffddeb9daf7e1
Pavlo Golub <pavlo.golub@gmail.com>
2021-02-11 11:33:16 +0100

    cleanup unused result tests

--- result_test.go
+++ result_test.go
@@ -1,36 +1,8 @@
 package pgxmock
 
 import (
-	"context"
-	"fmt"
 	"testing"
 )
-
-// used for examples
-var mock = &pgxmock{}
-
-func ExampleNewErrorResult() {
-	mock, _ := NewConn()
-	result := NewErrorResult(fmt.Errorf("some error"))
-	mock.ExpectExec("^INSERT (.+)").WillReturnResult(result)
-	res, _ := mock.Exec(context.Background(), "INSERT something")
-	s := res.String()
-	fmt.Println(s)
-	// Output: some error
-}
-
-func ExampleNewResult() {
-	var affected int64
-	result := NewResult("INSERT", affected)
-	mock.ExpectExec("^INSERT (.+)").WillReturnResult(result)
-	fmt.Println(mock.ExpectationsWereMet())
-	// Output: there is a remaining expectation which was not matched: ExpectedExec => expecting Exec or ExecContext which:
-	//   - matches sql: '^INSERT (.+)'
-	//   - is without arguments
-	//   - should return Result having:
-	//       LastInsertId: 0
-	//       RowsAffected: 0
-}
 
 func TestShouldReturnValidSqlDriverResult(t *testing.T) {
 	result := NewResult("SELECT", 2)
