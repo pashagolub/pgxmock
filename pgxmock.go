b6c7002c66ba115ba08c0a018775c5f3be67cef3
Pavlo Golub <pavlo.golub@gmail.com>
2021-06-02 11:17:12 +0300

    [+] add QueryFunc() to pgxIface

--- pgxmock.go
+++ pgxmock.go
@@ -113,8 +113,9 @@ type pgxIface interface {
 	Begin(context.Context) (pgx.Tx, error)
 	BeginTxFunc(ctx context.Context, txOptions pgx.TxOptions, f func(pgx.Tx) error) (err error)
 	Exec(context.Context, string, ...interface{}) (pgconn.CommandTag, error)
-	QueryRow(context.Context, string, ...interface{}) pgx.Row
 	Query(context.Context, string, ...interface{}) (pgx.Rows, error)
+	QueryRow(context.Context, string, ...interface{}) pgx.Row
+	QueryFunc(ctx context.Context, sql string, args []interface{}, scans []interface{}, f func(pgx.QueryFuncRow) error) (pgconn.CommandTag, error)
 	Ping(context.Context) error
 	Prepare(context.Context, string, string) (*pgconn.StatementDescription, error)
 	Deallocate(ctx context.Context, name string) error
