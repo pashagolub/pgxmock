90da7fd6f23cdfe99092d488020bbfec99673961
Pavlo Golub <pavlo.golub@gmail.com>
2022-03-11 17:15:09 +0100

    [+] improve test coverage

--- argument_test.go
+++ argument_test.go
@@ -20,7 +20,6 @@ func TestAnyTimeArgument(t *testing.T) {
 	if err != nil {
 		t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
 	}
-	// defer db.Close()
 
 	mock.ExpectExec("INSERT INTO users").
 		WithArgs("john", AnyTime{}).
@@ -42,12 +41,32 @@ func TestByteSliceArgument(t *testing.T) {
 	if err != nil {
 		t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
 	}
-	// defer db.Close()
 
 	username := []byte("user")
 	mock.ExpectExec("INSERT INTO users").WithArgs(username).WillReturnResult(NewResult("INSERT", 1))
 
 	_, err = mock.Exec(context.Background(), "INSERT INTO users(username) VALUES (?)", username)
+	if err != nil {
+		t.Errorf("error '%s' was not expected, while inserting a row", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestAnyArgument(t *testing.T) {
+	t.Parallel()
+	mock, err := NewConn()
+	if err != nil {
+		t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	mock.ExpectExec("INSERT INTO users").
+		WithArgs("john", AnyArg()).
+		WillReturnResult(NewResult("INSERT", 1))
+
+	_, err = mock.Exec(context.Background(), "INSERT INTO users(name, created_at) VALUES (?, ?)", "john", time.Now())
 	if err != nil {
 		t.Errorf("error '%s' was not expected, while inserting a row", err)
 	}
