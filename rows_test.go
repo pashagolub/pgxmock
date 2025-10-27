pashagolub/pgxmock

commit d07ccececf83b75cee6219be118d0fb230acdfe7

[+] teach `Rows.CloseError` to set the error (#241)
`Rows.Close` doesn't return an error, so up to now `Rows.CloseError` was
a no-op. However, pgx's `Rows.Err` can return an error after `Rows.Next`
returns false, or after `Rows.Close` has been called.

Add `rowSets.close()` to mark the current rows closed, jump the record
number to the last row, and to set the close error for that row. Then
have `rowSets.Close()` call it and `rowSets.Next()` call it once it has
processed the last row. These changes cause both `Rows.Err` and
`Rows.Scan` to return the close error once it has been set. Add tests
for this behavior.

While at it, have `Rows.Scan` return immediately when the rows have been
closed, rather than carrying out all the scanning.

Add tests to test these behaviors.

Fixes #238.

Authors:
*   asayapini <asayapini@ozon.ru>
*   David E. Wheeler <david@justatheory.com>

rows.go
@@ -82,12 +82,25 @@ func (rs *rowSets) Close() {
 	if rs.ex != nil {
 		rs.ex.rowsWereClosed = true
 	}
-	// return rs.sets[rs.pos].closeErr
+	rs.close()
+}
+
+// close marks the current rows closed, jumps to the last row, and sets the
+// close error.
+func (rs *rowSets) close() {
+	r := rs.sets[rs.RowSetNo]
+	r.recNo = len(r.rows)
+	r.nextErr[r.recNo-1] = r.closeErr
+	r.closed = true
 }
 
 // advances to next row
 func (rs *rowSets) Next() bool {
 	r := rs.sets[rs.RowSetNo]
+	if r.recNo == len(r.rows) {
+		rs.close()
+		return false
+	}
 	r.recNo++
 	return r.recNo <= len(r.rows)
 }
@@ -102,6 +115,11 @@ func (rs *rowSets) Values() ([]interface{}, error) {
 
 func (rs *rowSets) Scan(dest ...interface{}) error {
 	r := rs.sets[rs.RowSetNo]
+	if r.closed {
+		// If there is no error, we should return one anyway. Weirdly, pgx returns
+		// `number of field descriptions must equal number of values, got %d and %d`.
+		return r.nextErr[r.recNo-1]
+	}
 	if len(dest) == 1 {
 		if rc, ok := dest[0].(pgx.RowScanner); ok {
 			return rc.ScanRow(rs)
@@ -218,6 +236,7 @@ type Rows struct {
 	recNo      int
 	nextErr    map[int]error
 	closeErr   error
+	closed     bool
 }
 
 // NewRows allows Rows to be created from a
@@ -235,13 +254,8 @@ func NewRows(columns []string) *Rows {
 	}
 }
 
-// CloseError allows to set an error
-// which will be returned by rows.Close
-// function.
-//
-// The close error will be triggered only in cases
-// when rows.Next() EOF was not yet reached, that is
-// a default sql library behavior
+// CloseError sets an error which will be returned by [Rows.Err] after
+// [Rows.Close] has been called or [Rows.Next] returns false.
 func (r *Rows) CloseError(err error) *Rows {
 	r.closeErr = err
 	return r

rows_test.go
@@ -312,7 +312,7 @@ func TestAllowsToSetRowsErrors(t *testing.T) {
 	}
 }
 
-func TestRowsCloseError(t *testing.T) {
+func TestNoRowsCloseError(t *testing.T) {
 	t.Parallel()
 	mock, err := NewConn()
 	if err != nil {
@@ -331,6 +331,97 @@ func TestRowsCloseError(t *testing.T) {
 
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestRowsCloseError(t *testing.T) {
+	t.Parallel()
+	mock, err := NewConn()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mock.Close(context.Background())
+
+	rows := NewRows([]string{"id"}).AddRow(1).AddRow(2).CloseError(fmt.Errorf("close error"))
+	mock.ExpectQuery("SELECT").WillReturnRows(rows)
+
+	rs, err := mock.Query(context.Background(), "SELECT")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	defer rs.Close()
+
+	total := 0
+	for rs.Next() {
+		var id int
+		if err := rs.Scan(&id); err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
+		total += id
+	}
+
+	if total != 3 {
+		t.Fatalf("expected id sum of 3, got %d", total)
+	}
+
+	if rs.Err() == nil {
+		t.Fatal("expected an error, but got none")
+	}
+}
+
+func TestRowsCloseEarlyError(t *testing.T) {
+	t.Parallel()
+	mock, err := NewConn()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mock.Close(context.Background())
+
+	rows := NewRows([]string{"id"}).AddRow(1).AddRow(2).CloseError(fmt.Errorf("close error"))
+	mock.ExpectQuery("SELECT").WillReturnRows(rows)
+
+	rs, err := mock.Query(context.Background(), "SELECT")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	// Fetch the first row.
+	if !rs.Next() {
+		t.Fatal("unexpected false Next()")
+	}
+	var id int
+	if err := rs.Scan(&id); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if id != 1 {
+		t.Fatalf("expected id to be 1, got %d", id)
+	}
+
+	// Close before fetching the next row.
+	rs.Close()
+
+	// The close error should be set.
+	if rs.Err() == nil {
+		t.Fatal("expected an error, but got none")
+	}
+
+	// Next should be false now.
+	if rs.Next() {
+		t.Fatal("unexpected true Next()")
+	}
+
+	// Scan should return the error.
+	id = -1
+	if err := rs.Scan(&id); err == nil {
+		t.Fatal("expected an error, but got none")
+	}
+	if id != -1 {
+		t.Fatalf("expected no id but got %v", id)
+	}
+
+	// The close error should be set.
+	if rs.Err() == nil {
+		t.Fatal("expected an error, but got none")
 	}
 }
 

commit 5ddb2e86f394d1a2cd69e3df66827beb8a65f003

Allow RowSet with error with no corresponding row
Allow a RowSet to be specified with an error row number that has no
corresponding row. Since the row returns an error, it seems unnecessary
for there to be a row.

In other words, rather than require something like:

	NewRows([]string{"id", "title"}).
		AddRow("one").
		RowError(0, fmt.Errorf("error"))

In which both a row and a row error specify index 0, but the row is
never returned, allow omission of the row:

	NewRows([]string{"id", "title"}).
		RowError(0, fmt.Errorf("error"))

The pattern was broken in d07ccec, which checked only the number of rows
before closing a RowSet. Fix by having `Next` check both the length of
rows and whether an error is registered for the current row number. Add
a test for this case, as well.

rows.go
@@ -97,7 +97,7 @@ func (rs *rowSets) close() {
 // advances to next row
 func (rs *rowSets) Next() bool {
 	r := rs.sets[rs.RowSetNo]
-	if r.recNo == len(r.rows) {
+	if r.recNo == len(r.rows) && r.nextErr[r.recNo] == nil {
 		rs.close()
 		return false
 	}

rows_test.go
@@ -312,6 +312,36 @@ func TestAllowsToSetRowsErrors(t *testing.T) {
 	}
 }
 
+func TestOneRowError(t *testing.T) {
+	t.Parallel()
+	mock, err := NewConn()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mock.Close(context.Background())
+
+	rows := NewRows([]string{"id", "title"}).
+		RowError(0, fmt.Errorf("error"))
+	mock.ExpectQuery("SELECT").WillReturnRows(rows)
+
+	rs, err := mock.Query(context.Background(), "SELECT")
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	defer rs.Close()
+
+	if rs.Next() {
+		t.Fatal("expected the first row not to be available")
+	}
+	if rs.Err() == nil {
+		t.Fatalf("unexpected success")
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestNoRowsCloseError(t *testing.T) {
 	t.Parallel()
 	mock, err := NewConn()